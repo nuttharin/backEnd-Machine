6d3046ec25214330fb15660c1e3a71d7f32e9c98
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-12-04 15:23:08 +0700

    Update index.py

--- index.py
+++ index.py
@@ -7,32 +7,19 @@ from pclController import *
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
-# @app.route("/test" , methods=['GET'])
-# def test():
-#     return jsonify({'tasks': "test api flask"})
+@app.route("/test" , methods=['GET'])
+def test():
+    return jsonify({'tasks': "test api flask"})
 
-# @app.route("/test", methods=['POST'])
-# def test1():
-#     return  getIp()
+@app.route("/test", methods=['POST'])
+def test1():
+    return  getIp()
 
-# @app.route("/testplc", methods=['GET'])
-# testPCL()
+@app.route("/testplc", methods=['GET'])
+def testplc():
+    testPCL()
 
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-    
-    def post(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(TodoSimple, '/test')
-api.add_resource(TodoSimple, '/<string:todo_id>')
 
 
 
