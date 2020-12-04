33a45213efc1114f49aacc1bfdaf81e56148a1a5
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2020-12-04 15:08:21 +0700

    1

--- env.py
+++ env.py
@@ -1,3 +1,11 @@
+ip = "192.168.1.156"
+ipPLC = "192.168.250.1"
+portPLC = 502
 def getIp():
-    ip = "0.0.0.0.0"
     return ip
+
+def getIpPLC():
+    return ipPLC
+
+def getPortPLC():
+    return portPLC
--- index.py
+++ index.py
@@ -1,18 +1,42 @@
-from flask import Flask , jsonify
-from flask_restful import Api
+from flask import Flask , jsonify , request
+from flask_restful import Api ,Resource
 from env import *
+from pclController import *
 
-print(getIp())
+
 app = Flask(__name__)
 api = Api(app)
 
-@app.route("/test" , methods=['GET'])
-def test():
-    return jsonify({'tasks': "test api flask"})
+todos = {}
+# @app.route("/test" , methods=['GET'])
+# def test():
+#     return jsonify({'tasks': "test api flask"})
+
+# @app.route("/test", methods=['POST'])
+# def test1():
+#     return  getIp()
+
+# @app.route("/testplc", methods=['GET'])
+# testPCL()
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+    
+    def post(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/test')
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0" ,debug=True , port=80)
-    app.run(debug=True , port=5000)
+    app.run(host=getIp() ,debug=True , port=5000)
+   # app.run(debug=True , port=5000)
 print("ddd")
