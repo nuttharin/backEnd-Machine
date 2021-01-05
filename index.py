247f939533e1422c2bfbd0a3e0a42dd31d254bd0
nut_tharin <37585893+nuttharin@users.noreply.github.com>
2021-01-05 16:49:13 +0700

    test

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from flask_restful import Api ,Resource
 
 from env import *
 from pclController import *
+from command import *- 
 from pyModbusTCP.client import ModbusClient
 import requests
 
@@ -23,6 +24,9 @@ def test():
 @app.route("/test", methods=['POST'])
 def test1():
     return  getIp()
+
+
+
 
 @app.route("/testplc", methods=['GET'])
 def testplc():
@@ -105,7 +109,64 @@ def machineCommandGasOut():
         
            
 
+@app.route("/machine/command/test" , methods = ['POST'])
+def machineCommandGasOut():
+    # return jsonify({"data" : request.form['number_order']})    
+    command_str = request.json['command_str']
+    coil_number = testPCL(command_str)
+    print(coil_number)
+    # number_order = request.json['number_order']
+    # order_id = request.json['order_id']
+    # print(number_order)
+    # print(order_id)
+    # if number_order is None:
+    #     return  jsonify({
+    #             "status": "error",
+    #             "statusCode": 200,
+    #             "data" : "not have parameter ( number_order )"
+    #         })
+    # else:        
+    #     # c = ModbusClient(host=getIpPLC(),port=getPortPLC(),auto_open=True)
+    #     i = 1
+    #     while i <= number_order :
+    #         print("status write") 
+    #         is_ok = True                   
+    #         # is_ok = c.write_single_coil(0,1)
+    #         # print(c)            
+    #         # print(is_ok)
+           
+    #         if is_ok : 
+    #             # api update
+    #             # print("if is_ok")
+    #             url = "http://"+getIpApi()+"/app/post/fromMachine/update/quality/gasOut"
+    #             myobj = {
+    #                         'order_id': order_id ,  
+    #                         'quality' : i
+    #                     }
+    #             resJson = requests.post(url, data = myobj)
+    #             # print(i)
+    #             # print(number_order)
+    #             # print(resJson.json()["statusCode"])
 
+    #             if resJson.json()["statusCode"] == 201 : 
+    #                 # print("update complete")
+    #                 i+=1
+
+    #             if i == number_order :
+    #                 return  jsonify({
+    #                         "status": "success",
+    #                         "statusCode": 201,
+    #                         "data" : "write_single_coil complete"
+    #                     })
+    #             # i+=1
+    #         else :
+    #             print("else is_ok")
+    #             return  jsonify({
+    #                 "status": "error",
+    #                 "statusCode": 200,
+    #                 "data" : "error write_single_coil"
+    #             })
+        
 
 
 
